7802dbc333c433ea007d922aa2133a18a1371dd8
rap9210 <rap9210@gmail.com>
2020-04-17 16:26:09 -0400

    Guessing Game Fixed

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -4,13 +4,19 @@ public class GuessingGame {
         Scanner scn = new Scanner(System.in);
         System.out.println("Im thinking of a number between" +
                 " 1 and 10... can you guess what it is?");
-        int correct_guess = 10;
+        int correct_guess = 6;
         int guess_input = 0;
         while (guess_input != correct_guess){
             guess_input = scn.nextInt();
             if (guess_input != correct_guess){
-                System.out.println("Sorry! Try again!");
-            }
+                if (guess_input > correct_guess) {
+                    System.out.println("Sorry! That's too high. Try again!");
+                }
+                else if(guess_input < correct_guess){
+                    System.out.println("Sorry! That's too low. Try again!");
+                    }
+                }
+
             else{
                 System.out.println("You got it! Way to go.");
             }
